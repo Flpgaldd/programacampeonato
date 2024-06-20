5029936666a1e3e42ffbd20e215ce860c5a948cd
Felipe Galdino <feligald@gmail.com>
2024-06-20 20:32:02 -0300

    Pronto

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -36,12 +36,28 @@ class ChampionsController < ApplicationController
     end
 
     def edit
+      @champion = Champion.find(params[:id])
     end
 
+    def update
+      if @champions.update(championship_params)
+        redirect_to @champions, notice: 'Campeonato atualizado com sucesso.'
+      else
+        render :edit
+      end
+    end
     private
 
     def championship_params
       params.require(:champion).permit(:name, :start_date, :end_date, :game_type, :vision, :pontuation, :team, :match_total, :description, :rules, :premiation, :image )
     end
 
+    def set_champion
+      @champion = Champion.find(params[:id])
+    end
+
+    def correct_user
+      @champion = current_user.champions.find_by(id: params[:id])
+      redirect_to champions_path, notice: "Você não tem permissão para editar este campeonato" if @champion.nil?
+    end
 end
